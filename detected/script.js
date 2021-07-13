9d219738e718a2958f7742c19bfd2f6a328682f4
Nikita Smelkov <nikiboy2001rus@gmail.com>
2021-07-13 15:39:32 +0300

    Detected / vibration 4

--- detected/script.js
+++ detected/script.js
@@ -84,7 +84,7 @@ function handleClick() {
     audioPlayed = true;
   }
 
-  if (canVibrate) window.navigator.vibrate(20000);
+  if (canVibrate) window.navigator.vibrate(200);
 
   c1 = colors[getRandomInt()];
   c2 = colors[getRandomInt()];
