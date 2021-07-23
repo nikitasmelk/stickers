61e8d7f31310f1c1068dc55fb2c9e504a731f357
Nikita Smelkov <nikiboy2001rus@gmail.com>
2021-07-23 23:20:58 +0300

    Testing smile features

--- smile/script.js
+++ smile/script.js
@@ -1,4 +1,5 @@
 var gif = document.getElementById("gif");
+var bg = document.getElementById("backgroundVideo");
 let swapped = false;
 
 function ready() {
@@ -7,6 +8,20 @@ function ready() {
   setTimeout(function() {
     gif.src = "animation_loop.gif";
   }, 23000);
+}
+
+function handleClick(){
+  console.log("click");
+  if(!swapped){
+    gif.classList.add("invert");
+    bg.classList.add("invert");
+    swapped = true;
+  } else {
+    gif.classList.remove("invert");
+    bg.classList.remove("invert");
+    swapped = false;
+  }
+  
 }
 
 document.addEventListener("DOMContentLoaded", ready);
