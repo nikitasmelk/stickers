fadf8a15571d36d07d89a5887a5b8a1b14a8b278
Nikita Smelkov <nikiboy2001rus@gmail.com>
2021-11-11 19:48:57 -0800

    Portal optimizing 16

--- portal/script.js
+++ portal/script.js
@@ -1,3 +1,4 @@
+
 let colors = [
   "#63b598", "#ce7d78", "#ea9e70", "#a48a9e", "#c6e1e8", "#648177" ,"#0d5ac1" ,
   "#f205e6" ,"#1c0365" ,"#14a9ad" ,"#4ca2f9" ,"#a4e43f" ,"#d298e2" ,"#6119d0",
@@ -71,9 +72,9 @@ for (let i = 0; i < letters.length; i++) {
   //   event.target.style.color = 'black';
   // }, false);
 
-  document.addEventListener('swiped', function(e) {
-    e.target.style.color = array_of_colors[getRandomInt(array_of_colors.length)];
-});
+//   document.addEventListener('swiped', function(e) {
+//     e.target.style.color = array_of_colors[getRandomInt(array_of_colors.length)];
+// });
 
   // letters[i].addEventListener('swiped', function(e) {
   //   e.target.style.color = array_of_colors[getRandomInt(array_of_colors.length)];
